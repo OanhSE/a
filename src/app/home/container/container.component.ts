2395029f1bfc4f651fbdc7bf09fae24f9e594c3d
OanhSE <vothituoanh2911@gmail.com>
2021-05-22 23:20:05 +0700

    layout

--- src/app/home/container/container.component.ts
+++ src/app/home/container/container.component.ts
@@ -7,8 +7,10 @@ import { Film } from 'src/app/_models/Film';
 import {Router} from '@angular/router';
 @Component({
   selector: 'app-container',
-  templateUrl: './container.component.html',
-  styleUrls: ['./container.component.css']
+  // templateUrl: './container.component.html',
+  // styleUrls: ['./container.component.css']
+  templateUrl: './container.components.html',
+  styleUrls: ['./container.components.css']
 })
 export class ContainerComponent implements OnInit {
 
